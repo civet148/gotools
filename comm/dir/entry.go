da41b00eaa8b88e0a235848826f8fb59f631abdc
libin <civet148@126.com>
2020-02-17 22:10:07 +0800

    [dir] add time to FileEntry/DirEntry

--- comm/dir/entry.go
+++ comm/dir/entry.go
@@ -1,6 +1,9 @@
 package dir
 
-import "os"
+import (
+	"os"
+	"time"
+)
 
 type FileEntry struct {
 	Name     string
@@ -8,12 +11,14 @@ type FileEntry struct {
 	PathName string
 	Mode     os.FileMode
 	Size     int64
+	Time     time.Time
 }
 
 type DirEntry struct {
 	Name     string
 	DirName  string
 	PathName string
+	Time     time.Time
 	files    []*FileEntry
 	dirs     []*DirEntry
 }
@@ -26,6 +31,7 @@ func newFileEntry(dirPath, pathSep string, f os.FileInfo) (e *FileEntry) {
 		PathName: dirPath + pathSep + f.Name(),
 		Size:     f.Size(),
 		Mode:     f.Mode(),
+		Time:     f.ModTime(),
 	}
 }
 
@@ -34,6 +40,7 @@ func newDirEntry(dirPath, pathSep string, f os.FileInfo) (e *DirEntry) {
 		Name:     f.Name(),
 		DirName:  dirPath,
 		PathName: dirPath + pathSep + f.Name(),
+		Time:     f.ModTime(),
 	}
 }
 
--- comm/dir/test/main.go
+++ comm/dir/test/main.go
@@ -10,9 +10,9 @@ func main() {
 	//测试正则表达式查找/tmp目录下xxx.log文件以及 xxx.log.20200217_150326类似的日志备份文件
 	entry, _ := dir.GetFilesAndDirs("/tmp", ".log", ".\\d{8}_\\d{6}")
 	for _, v := range entry.GetFiles() {
-		log.Info("files [%#v]", v)
+		log.Json("files", v)
 	}
 	for _, v := range entry.GetDirs() {
-		log.Info("dirs [%#v]", v)
+		log.Json("dirs", v)
 	}
 }
