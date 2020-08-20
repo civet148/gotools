e6f22b1b977880b7a094351f28b73008881fb075
libin <civet148@126.com>
2020-08-20 16:15:20 +0800

    [gotools] add MQ consumer and producer test case

---
+++ mq/test/consumer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/civet148/gotools/log"
+	"github.com/civet148/gotools/mq"
+	_ "github.com/civet148/gotools/mq/rabbit"
+)
+
+type ConsumerHandler struct {
+}
+
+func main() {
+	var err error
+	//var strTopicRoutingKey = "TOPIC.NEWS.ROUTINGKEY"
+	var strTopicBindingKey = "TOPIC.NEWS.#" //#表示一个或多个单词（以.分割），*表示一个单词
+	var strQueueName = "TOPIC.QUEUE"
+
+	strConnUrl := "amqp://127.0.0.1:5672"
+	rabbitMQ, _ := mq.GetAdapter(mq.Adapter_RabbitMQ)
+	if err = rabbitMQ.Connect(mq.Mode_Topic, strConnUrl); err != nil {
+		log.Errorf("connect to MQ broker error [%v]", err.Error())
+		return
+	}
+
+	_ = TopicConsumer(rabbitMQ, strTopicBindingKey, strQueueName)
+}
+
+func (c *ConsumerHandler) OnConsume(adapter mq.Adapter, strBindingKey, strQueueName, strKey, strValue string) {
+	log.Infof("[%+v] binding key [%v] queue name [%v] message key [%+v] value [%+v]", adapter, strBindingKey, strQueueName, strKey, strValue)
+}
+
+func TopicConsumer(rabbitMQ mq.IReactMQ, strBindingKey, strQueueName string) (err error) {
+	err = rabbitMQ.Consume(strBindingKey, strQueueName, &ConsumerHandler{})
+	if err != nil {
+		log.Error("%v", err.Error())
+		return
+	}
+	return
+}
---
+++ mq/test/producer/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/civet148/gotools/log"
+	"github.com/civet148/gotools/mq"
+	_ "github.com/civet148/gotools/mq/rabbit"
+	"time"
+)
+
+func main() {
+	var err error
+	var strTopicRoutingKey = "TOPIC.NEWS.ROUTINGKEY"
+	//var strTopicBindingKey = "TOPIC.NEWS.#" //#表示一个或多个单词（以.分割），*表示一个单词
+	var strQueueName = "TOPIC.QUEUE"
+
+	strConnUrl := "amqp://127.0.0.1:5672"
+	rabbitMQ, _ := mq.GetAdapter(mq.Adapter_RabbitMQ)
+	if err = rabbitMQ.Connect(mq.Mode_Topic, strConnUrl); err != nil {
+		log.Errorf("connect to MQ broker error [%v]", err.Error())
+		return
+	}
+
+	_ = TopicProducer(rabbitMQ, strTopicRoutingKey, strQueueName)
+}
+
+func TopicProducer(ReactMQ mq.IReactMQ, strBindingKey, strQueueName string) (err error) {
+
+	var bConnDown bool //MQ服务器异常
+	var nMsgIndex int
+
+	var strKey = mq.DEFAULT_DATA_KEY
+
+	prod := ReactMQ
+	for {
+
+		if bConnDown { //服务器异常宕机或重启
+			//ReactMQ.Connect(mq.Mode_Topic, )
+		}
+		var strData string = "This is topic data"
+		strMsg := fmt.Sprintf("%v[%v]", strData, nMsgIndex)
+		if err = prod.Publish(strBindingKey, strQueueName, strKey, strMsg); err != nil {
+			log.Error("%v", err.Error())
+			bConnDown = true
+			goto CONTINUE
+		}
+		log.Info("Publish [topic] data [%v] to broker ok", strMsg)
+		nMsgIndex++
+
+	CONTINUE:
+		if prod.IsClosed() {
+			if err = prod.Reconnect(); err != nil { //重新做一次连接尝试
+				log.Error("Reconnect to MQ server error [%v]", err.Error())
+			} else {
+				log.Info("Reconnect to MQ server ok")
+			}
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
