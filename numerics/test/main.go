f3d2d9805125bac57fe41f38db09a15eebc26263
libin <civet148@126.com>
2020-08-20 20:02:14 +0800

    [gotools] numeric integer add

--- numerics/test/main.go
+++ numerics/test/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/civet148/gotools/log"
+	"fmt"
 	"github.com/civet148/gotools/numerics"
 )
 
@@ -9,6 +9,6 @@ func main() {
 	var ints = []uint32{123, 124, 125, 126}
 	var floats = []float32{123.1, 124.2, 125.3, 126.4}
 
-	log.Infof("numeric int join [%+v]", numerics.Join(ints, ","))
-	log.Infof("numeric float join [%+v]", numerics.Join(floats, ","))
+	fmt.Printf("numeric int join [%+v] \n", numerics.Join(ints, ","))
+	fmt.Printf("numeric float join [%+v] \n", numerics.Join(floats, ","))
 }
