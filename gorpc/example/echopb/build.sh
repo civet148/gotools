ec2b6ee318dac8fe6b1313f02acc74706a1b6d2d
libin <civet148@126.com>
2020-09-10 14:43:44 +0800

    [gotools] fix gorpc build shell script

--- gorpc/example/echopb/build.sh
+++ gorpc/example/echopb/build.sh
@@ -1,2 +1,2 @@
 ### protobuf generation with protoc-gen-micro and protoc-gen-go
-protoc -I=.  --proto_path=$GOPATH\src  --micro_out=.  --go_out=.   *.proto
+protoc -I=.  --proto_path=$GOPATH/src  --micro_out=.  --go_out=.   *.proto
