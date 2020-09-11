5e3ca30f033ed9dce1c9fffc66c7df4f8e1a2787
libin <civet148@126.com>
2020-09-11 10:31:03 +0800

    [gotools/cryptos] gomd5 test case update

--- cryptos/gomd5/test/main.go
+++ cryptos/gomd5/test/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/civet148/gotools/cryptos/gomd5"
-	"github.com/civet148/gotools/process"
+	"os"
 )
 
-var data = "12312431243543645666666666666666666666354345"
-
 func main() {
-	gopath := process.GetEnv("GOPATH")
-	if gopath != "" {
-		strMd5 := gomd5.Md5File(fmt.Sprintf("%s/src/github.com/civet148/gotools/cryptos/gomd5/gomd5.go", gopath))
-		fmt.Printf("Md5File -> [%v]\n", strMd5)
+
+	var strFile string
+	if len(os.Args) != 2 {
+		fmt.Println("please input file path")
+		return
 	}
-	strSum := gomd5.Md5Sum([]byte(data))
-	fmt.Printf("Md5Sum  -> [%v]\n", strSum)
+
+	strFile = os.Args[1]
+	strMd5 := gomd5.Md5File(strFile)
+	fmt.Printf("Md5File -> [%v]\n", strMd5)
 }
