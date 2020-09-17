ee67f7fe52cbc8447f4fe2857d68915bf5c4340d
libin <civet148@126.com>
2020-09-17 16:35:50 +0800

    [timer] bug fixed

--- comm/timer/Timer.go
+++ comm/timer/Timer.go
@@ -13,7 +13,7 @@ var (
 )
 
 const (
-	TICKER_INTERVAL_MS = 100
+	TIKER_INTERVAL_MS = 500
 )
 
 type ITimer interface {
@@ -23,12 +23,12 @@ type ITimer interface {
 
 //定时任务内部对象
 type timerInner struct {
-	this   interface{} //需要执行定时任务的对象指针
-	id     int         //定时任务ID
-	elapse int         //定时任务执行时间间隔(单位: 毫秒)
-	param  interface{} //参数
-	repeat int         //重复次数
-	msec   int64       //下次执行时间(毫秒)
+	this    interface{} //需要执行定时任务的对象指针
+	id      int         //定时任务ID
+	elapse  int         //定时任务执行时间间隔(单位: 毫秒)
+	param   interface{} //参数
+	repeat  int         //重复次数
+	closing chan bool   //关闭消息
 }
 
 var gMapTimer = sync.Map{}
@@ -36,60 +36,6 @@ var gMapTimer = sync.Map{}
 //初始化
 func init() {
 
-	go startMonitor() //启动定时任务监视协程
-}
-
-func startMonitor() {
-
-	ticker := time.NewTicker(TICKER_INTERVAL_MS * time.Millisecond) //n毫秒触发一次
-
-	for {
-		select {
-		case <-ticker.C: //遍历一次需要执行的定时任务
-			gMapTimer.Range(
-				func(key, value interface{}) bool {
-
-					nCurMs := getCurTime()
-
-					inner := value.(*timerInner)
-
-					if nCurMs >= inner.msec {
-
-						//log.Debug("key [%v] value [%+v] cur ms[%v] duration [%v]", key, value, nCurMs, nCurMs-inner.msec)
-						cb := inner.this.(ITimer)
-						go cb.OnTimer(inner.id, inner.param) //调用OnTimer方法执行定时任务
-						if inner.repeat != RepeatForever && inner.repeat != RepeatDone {
-							inner.repeat-- //计数器减一
-						}
-
-						if inner.repeat == RepeatDone { //计数为0，删除定时任务对象
-							gMapTimer.Delete(key)
-						} else {
-							inner.msec = getNextTime(inner.elapse) //设置下一次执行时间
-						}
-					}
-
-					return true
-				},
-			)
-		}
-	}
-
-}
-
-//获取当前时间(毫秒)
-func getCurTime() (msec int64) {
-
-	now := time.Now()
-	return now.UnixNano() / 1e6
-}
-
-//获取下次执行时间(毫秒)
-func getNextTime(elapse int) (msec int64) {
-
-	now := time.Now()
-	msec = now.UnixNano()/1e6 + int64(elapse)
-	return
 }
 
 func getTimerKey(this interface{}, id int) string {
@@ -109,25 +55,52 @@ func SetTimer(this interface{}, id int, elapse int, repeat int, param interface{
 		return false
 	}
 
-	if elapse < TICKER_INTERVAL_MS {
-		elapse = TICKER_INTERVAL_MS
+	if elapse < TIKER_INTERVAL_MS {
+		elapse = TIKER_INTERVAL_MS
 	}
 
 	strKey := getTimerKey(this, id)
-	inner := timerInner{
-		this:   this,
-		id:     id,
-		elapse: elapse,
-		repeat: repeat,
-		param:  param,
-		msec:   getNextTime(elapse), //首次执行时间（毫秒）
+	inner := &timerInner{
+		this:    this,
+		id:      id,
+		elapse:  elapse,
+		repeat:  repeat,
+		param:   param,
+		closing: make(chan bool, 1),
 	}
-
-	gMapTimer.Store(strKey, &inner)
+	gMapTimer.Store(strKey, inner)
+	go startTimer(inner)
 	return true
 }
 
 //停止定时任务
 func KillTimer(this interface{}, id int) {
-	gMapTimer.Delete(getTimerKey(this, id))
+	var strKey = getTimerKey(this, id)
+	if v, ok := gMapTimer.Load(strKey); ok {
+		inner := v.(*timerInner)
+		inner.closing <- true
+		gMapTimer.Delete(strKey)
+	}
+}
+
+func startTimer(inner *timerInner) {
+	//log.Debugf("start timer inner id [%+v]", inner.id)
+	for {
+		select {
+		case <-inner.closing:
+			return
+		case <-time.After(time.Duration(inner.elapse) * time.Millisecond):
+			{
+				//log.Debugf("timer inner [%+v]", inner)
+				cb := inner.this.(ITimer)
+				cb.OnTimer(inner.id, inner.param) //调用OnTimer方法执行定时任务
+				if inner.repeat != RepeatForever && inner.repeat != RepeatDone {
+					inner.repeat-- //计数器减一
+				}
+				if inner.repeat == 0 {
+					return
+				}
+			}
+		}
+	}
 }
